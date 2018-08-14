27360780c6e1c6015ffd8d9267875eec59b33f9d
Lauren Sutton <laurensutton@172-29-233-243.DYNAPOOL.NYU.EDU>
2018-08-13 20:35:11 -0400

    adding

---
+++ hangman.py
@@ -0,0 +1,24 @@
+class Hangman():
+
+    def __init__(self, word, guesses=0, guessed_letters=[]):
+        self.word = word
+        self.guesses = guesses
+        self.guessed_letters = guessed_letters
+
+    def guesses_less_than_5(self, word, guesses, guessed_letters)
+
+
+    def set_intersection(self, word, guesses, guessed_letters):
+
+if set(word).intersection(set(guessed_letters)) == set(word):
+    print('You won!')
+else:
+    print('You lost!')
+
+def main():
+    magic_word = Hangman('dorito')
+    magic_word = guesses_less_than_5(word=magic_word.word, guesses=magic_word.guesses)
+    magic_word.set_intersection(word=magic_word.word, guesses=magic_word.guesses)
+
+if __name__ = "__main__"
+    main()
